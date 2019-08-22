ba64ac0e3f05699ca85d17e5cd202dd9e3f76ef7
Hakhyun Kim <hhkim94@naver.com>
2019-08-22 20:49:57 +0900

    p10871 added.

---
+++ implementation/p10871.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    //freopen("p10871_sample.txt", "r", stdin);
+
+    int N, X;
+    scanf("%d %d", &N, &X);
+
+    vector<int> ret;
+    for(int i=0; i<N; i++) {
+        int num;
+        cin >> num;
+        if(num < X) ret.push_back(num);
+    }
+
+    for(int i=0; i<ret.size(); i++) printf("%d ", ret[i]);
+    printf("\n");
+
+    return 0;
+}
