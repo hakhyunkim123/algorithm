e1b8a323c2f695fd7fd6b0e8e907688615d44142
Hakhyun Kim <hhkim94@naver.com>
2019-10-10 13:33:37 +0900

    complete search problem added.

---
+++ Problem/Brute Force/baseball.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<string> num_list;
+bool visited[10];
+vector<string> ret;
+
+bool check(const string num, const vector<vector<int>> baseball) {
+    bool ret = true;
+    for(int idx=0; idx<baseball.size(); idx++) {
+        int s = 0, b = 0;
+        string compare_number = to_string(baseball[idx][0]);
+        for(int i=0; i<3; i++) 
+            for(int j=0; j<3; j++) 
+                if(num[i] == compare_number[j]) i == j ? s++ : b++;
+     
+        if(baseball[idx][1] != s || baseball[idx][2] != b) {
+            ret = false;
+            break;
+        }
+    }
+    return ret;
+}
+
+void set_number(string num, int depth, vector<vector<int>> baseball) {
+    if(depth == 0) {
+        if(check(num, baseball)) ret.push_back(num);
+        return;
+    }
+    
+    for(int i=0; i<num_list.size(); i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            set_number(num + num_list[i], depth-1, baseball);
+            visited[i] = false;
+        }
+    }
+}
+
+int solution(vector<vector<int>> baseball) {
+    for(int i=1; i<10; i++) num_list.push_back(to_string(i));
+    
+    for(int i=0; i<num_list.size(); i++) {
+        memset(visited, false, sizeof(visited));
+        visited[i] = true;
+        set_number(num_list[i], 2, baseball);
+        visited[i] = false;
+    }
+    
+    int answer = ret.size();
+    return answer;
+}
---
+++ Problem/Brute Force/find_prime.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+bool visited[10];
+bool number[10000000];
+string in_str = "011";
+vector<string> decimal;
+
+bool is_prime(int num) {
+    if(num <= 1) return false;
+    
+    bool ret = true;
+    for(int i = 2; i < num; i++) 
+        if(num % i == 0) {
+            ret = false;
+            break;
+        }
+    
+    return ret;
+}
+
+int count_prime(string num, int depth, int last) {
+    if(depth > last) return 0;
+
+    int ret = 0;
+    int str_to_num = atoi(num.c_str());
+    if(!number[str_to_num] && is_prime(str_to_num)) ret++;
+    
+    number[str_to_num] = true;
+
+    for(int i=0; i<decimal.size(); i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            ret += count_prime(num + decimal[i], depth+1, last);
+            visited[i] = false;
+        }
+    }
+
+    return ret;
+}
+
+int main() {
+    for(int i=0; i<in_str.length(); i++) {
+        string num(1, in_str[i]);
+        decimal.push_back(num);
+    }
+
+    int ans = 0;
+    memset(number, false, sizeof(number));
+    for(int i=0; i<decimal.size(); i++) {
+        memset(visited, false, sizeof(visited));
+
+        visited[i] = true;
+        ans += count_prime(decimal[i], 1, decimal.size());
+        visited[i] = false;
+    }
+
+    cout << ans << endl;
+    
+    return 0;
+}
